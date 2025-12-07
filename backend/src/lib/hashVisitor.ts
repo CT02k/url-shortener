63ae05dfdca53e71937b75419495212939e18494
CT02k <joaopaulocfwork@gmail.com>
2025-12-07 00:24:19 -0300

    feat(backend): using only user ip in the visitor hash

--- backend/src/lib/hashVisitor.ts
+++ backend/src/lib/hashVisitor.ts
@@ -1,10 +1,9 @@
 import type { Request } from "express";
 import { createHash } from "crypto";
-import { getClientIp, getUserAgent } from "./requestInfo";
+import { getClientIp } from "./requestInfo";
 
 export default function getVisitorId(req: Request) {
   const ip = getClientIp(req) || "0.0.0.0";
-  const ua = getUserAgent(req) || "";
 
-  return createHash("sha256").update(`${ip}:${ua}`).digest("hex");
+  return createHash("sha256").update(ip).digest("hex");
 }
